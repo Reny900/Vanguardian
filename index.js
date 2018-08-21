e21b5247e80ce888e685ff67badf4c5f84ac3970
Yuyuko <40657629+Yuyuko-sama@users.noreply.github.com>
2018-08-21 14:27:54 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-// Version 1.2.5
-
 'use strict'
 
-module.exports = function Vanguardian(dispatch) {
-
+module.exports = function Vanguardian(mod) {
 	let questid = 0,
 		timeout = null,
 		timeoutdaily = null,
@@ -15,8 +12,7 @@ module.exports = function Vanguardian(dispatch) {
 	// ############# //
 	// ### Hooks ### //
 	// ############# //
-
-	dispatch.game.on('enter_game', () => {
+	mod.game.on('enter_game', () => {
 		questid = 0
 		daily = 0
 		weekly = 0
@@ -25,71 +21,87 @@ module.exports = function Vanguardian(dispatch) {
 		timeoutweekly = null
 	})
 
-	dispatch.hook('S_COMPLETE_EVENT_MATCHING_QUEST', 1, event => {
+	mod.hook('S_COMPLETE_EVENT_MATCHING_QUEST', 1, event => {
 		questid = event.id
-		if(questid != 0) timeout = setTimeout(CompleteQuest, 2000) // try to complete the quest after 2 seconds
+		if(questid != 0)
+            timeout = setTimeout(CompleteQuest, 2000) // try to complete the quest after 2 seconds
 		return false
 	})
 
-	dispatch.hook('S_AVAILABLE_EVENT_MATCHING_LIST', 2, event => {
+	mod.hook('S_AVAILABLE_EVENT_MATCHING_LIST', 2, event => {
 		daily = event.unk4
 		weekly = event.unk6
 	})
 
-
 	// ################# //
 	// ### Functions ### //
 	// ################# //
-
 	function CompleteQuest() {
 		clearTimeout(timeout)
-		if(!enabled) return
-		if(dispatch.game.me.alive && !dispatch.game.me.inBattleground) { // if alive and not in a battleground
-			dispatch.toServer('C_COMPLETE_DAILY_EVENT', 1, { id: questid })
+		if(!enabled)
+            return
+
+		if(mod.game.me.alive && !mod.game.me.inBattleground) {
+			mod.send('C_COMPLETE_DAILY_EVENT', 1, { id: questid })
 			questid = 0
+
 			if(daily < 16) {
 				daily++
 				weekly++
-				command.message('<font color="#E69F00">[H每日任务:]你今日已经完成：</font>' + `<font color="#56B4E9">${daily}</font>` + '<font color="#56B4E9">个</font>'+'<font color="#E69F00">每日任务</font>')
-			}
-			else command.message('<font color="#E69F00">[H每日任务:]你今日已经完成全部：</font>' + `<font color="#56B4E9">${daily}</font>` + '<font color="#56B4E9">个</font>'+'<font color="#E69F00">每日任务</font>')
-			if(daily == 3 || daily == 8) timeoutdaily = setTimeout(CompleteDaily, 1000)
-			if(weekly == 16) timeoutweekly = setTimeout(CompleteWeekly, 1500)
+				mod.command.message('<font color="#E69F00">[H每日任务:]你今日已经完成：</font>' + `<font color="#56B4E9">${daily}</font>` + '<font color="#56B4E9">个</font>'+'<font color="#E69F00">每日任务</font>');
+			} else {
+                mod.command.message('<font color="#E69F00">[H每日任务:]你今日已经完成全部：</font>' + `<font color="#56B4E9">${daily}</font>` + '<font color="#56B4E9">个</font>'+'<font color="#E69F00">每日任务</font>');
+            }
+
+			if(daily == 3 || daily == 8)
+                timeoutdaily = setTimeout(CompleteDaily, 1000)
+
+			if(weekly == 16)
+                timeoutweekly = setTimeout(CompleteWeekly, 1500)
 		}
-		else timeout = setTimeout(CompleteQuest, 5000) // if dead or busy, retry to complete quest after 5 seconds
+		else
+            timeout = setTimeout(CompleteQuest, 5000) // if dead or busy, retry to complete quest after 5 seconds
 	}
 
 	function CompleteDaily() {
 		clearTimeout(timeoutdaily)
-		if(!enabled) return
-		if(dispatch.game.me.alive && !dispatch.game.me.inBattleground) // if alive and not in a battleground
-			dispatch.toServer('C_COMPLETE_EXTRA_EVENT', 1, { type: 1 })
-		else timeoutdaily = setTimeout(CompleteDaily, 5000) // if dead or busy, retry to complete quest after 5 seconds
+		if(!enabled)
+            return
+
+		if(mod.game.me.alive && !mod.game.me.inBattleground)
+			mod.send('C_COMPLETE_EXTRA_EVENT', 1, { type: 1 })
+		else
+            timeoutdaily = setTimeout(CompleteDaily, 5000) // if dead or busy, retry to complete quest after 5 seconds
 	}
 
 	function CompleteWeekly() {
 		clearTimeout(timeoutweekly)
-		if(!enabled) return
-		if(dispatch.game.me.alive && !dispatch.game.me.inBattleground) // if alive and not in a battleground
-			dispatch.toServer('C_COMPLETE_EXTRA_EVENT', 1, { type: 0 })
-		else timeoutweekly = setTimeout(CompleteWeekly, 5000) // if dead or busy, retry to complete quest after 5 seconds
+		if(!enabled)
+            return
+
+		if(mod.game.me.alive && !mod.game.me.inBattleground)
+			mod.send('C_COMPLETE_EXTRA_EVENT', 1, { type: 0 })
+		else
+            timeoutweekly = setTimeout(CompleteWeekly, 5000) // if dead or busy, retry to complete quest after 5 seconds
 	}
 
 	// ################ //
 	// ### Commands ### //
 	// ################ //
-
-	command.add('vg', (param) => {
-		if(param == null) {
-			enabled = !enabled
-			command.message('<font color="#E69F00">[H每日任务:] </font>' + (enabled ? '<font color="#56B4E9">开启</font>' : '<font color="#E69F00">关闭</font>'))
-			console.log('[H每日任务:] ' + (enabled ? 'enabled' : 'disabled'))
-		}
-		else if(param == "show")
-			command.message('<font color="#E69F00">[H每日任务:]你今日已经完成：</font>' + `<font color="#56B4E9">${daily}</font>` + '<font color="#56B4E9">个</font>'+'<font color="#E69F00">每日任务</font>')
-		else command.message('Commands:<br>'
+	mod.command.add('vg', {
+		$default() {
+            mod.command.message('Commands:<br>'
 							+ ' "vg" (enable/disable Vanguardian),<br>'
 							+ ' "vg daily" (Tells you how many Vanguard Requests you completed today")'
-		)
+            );
+        },
+		$none() {
+			enabled = !enabled
+			mod.command.message('<font color="#E69F00">[H每日任务:] </font>' + (enabled ? '<font color="#56B4E9">开启</font>' : '<font color="#E69F00">关闭</font>'))
+			console.log('[H每日任务:] ' + (enabled ? 'enabled' : 'disabled'))
+		},
+		show() {
+			mod.command.message('<font color="#E69F00">[H每日任务:]你今日已经完成：</font>' + `<font color="#56B4E9">${daily}</font>` + '<font color="#56B4E9">个</font>'+'<font color="#E69F00">每日任务</font>')
+        },
 	})
 }
